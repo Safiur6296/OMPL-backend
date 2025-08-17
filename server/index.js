5248678e45f1d6a4096e78c8b591f2b39994fb34
Md Safiur Rahaman <mdsafiur.10831504@ltimindtree.com>
2025-08-17 20:19:40 +0530

    removing cros

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const Razorpay = require('razorpay');
 const crypto = require('crypto');
 require('dotenv').config();
 
 const app = express();
-app.use(cors({
-    origin: ['http://localhost:3000','https://ompl.netlify.app'] ,// Replace with your actual domain
-    methods: ['GET', 'POST'],
-    credentials: true
-}));
+// app.use(cors({
+//     origin: ['http://localhost:3000','https://ompl.netlify.app'] ,// Replace with your actual domain
+//     methods: ['GET', 'POST'],
+//     credentials: true
+// }));
 app.use(express.json());
 
 // Initialize Razorpay
